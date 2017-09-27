c02297d294dcf9c4db3b25254852b4dd080ccbdf
chandrashekhar <chandrashekhar@chandrashekhar-VirtualBox>
2017-09-27 14:04:57 -0700

    Publisher

--- i-MDSA/core/publisher/setup.js
+++
@@ -1,8 +0,0 @@
-/**
- * 
- * Device Communication
- *
- */
-
-const CREATE_DEVICE_DETAILS_SCHEMA="CREATE KEYSPACE DEVICE_DETAILS with Replication = { 'class':'SimpleStrategy',replication_factor:1}";
-const CREATE_DEVICE_STATUS_TABLE="CREATE TABLE DEVICE_STATUS (messageID text PRIMARY KEY, sourceID text, loggedat time, message text)";
